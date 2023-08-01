f69eb13406758bb48cdcaf5c421fb6ac6ab4764b
GHMatrix <gsmensah@yahoo.com>
2023-08-01 05:41:33 -0700

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,21 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_height - Measures the height of a binary tree.
  *
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: Pointer to the root node of the tree to measure the height of.
  *
- * Return: The height of the tree, or 0 if tree is NULL
+ * Return: Height of the tree. If tree is NULL, return 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	return (0);
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (0);
 
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	return (left_height + 1);
-	return (right_height + 1);
+	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
 }
