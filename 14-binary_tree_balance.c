2f9477591ee0af070bbe9b2ee84ae6cc678a4af9
GHMatrix <gsmensah@yahoo.com>
2023-08-02 04:26:05 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,10 +7,11 @@
  *
  * Return: Height of the tree. If tree is NULL, return 0.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(tree)
+	binary_tree_t *tree;
 {
 	if (!tree)
-		return (0);
+	return (0);
 
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
@@ -25,10 +26,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: Balance factor of the tree. If tree is NULL, return 0.
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_balance(tree)
+	binary_tree_t *tree;
 {
 	if (!tree)
-		return (0);
+	return (0);
 
 	int left_height = (int)binary_tree_height(tree->left);
 	int right_height = (int)binary_tree_height(tree->right);
