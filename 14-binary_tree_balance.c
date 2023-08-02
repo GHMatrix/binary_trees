ced37da89cf926034258755f6e4f8af849e5965d
GHMatrix <gsmensah@yahoo.com>
2023-08-02 06:21:50 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,39 +1,37 @@
+#include <stddef.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_height - Measures the height of a binary tree
  *
- * @tree: Pointer to the root node of the tree to measure the height of.
+ * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: Height of the tree. If tree is NULL, return 0.
+ * Return: Height of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_height(tree)
-	binary_tree_t *tree;
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	size_t left_height, right_height;
+
+	if (tree == NULL)
 	return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree.
+ * binary_tree_balance - Measures the balance factor of a binary tree
  *
- * @tree: Pointer to the root node of the tree to measure the balance factor.
+ * @tree: Pointer to the root node of the tree to measure the balance factor
  *
- * Return: Balance factor of the tree. If tree is NULL, return 0.
+ * Return: Balance factor of the tree, or 0 if tree is NULL
  */
-int binary_tree_balance(tree)
-	binary_tree_t *tree;
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 	return (0);
 
-	int left_height = (int)binary_tree_height(tree->left);
-	int right_height = (int)binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
